5badf4ed93a253295474749f01ccbf0911b97e8e
Guillaume <guillaume.denece@gmail.com>
2024-02-28 14:05:32 +0100

    add romm-finder-service

--- three-layers/tests/hello-world.test.ts
+++ three-layers/tests/hello-world.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from '@jest/globals';
-import { main } from 'three-layers/src/main.ts';
+import { main } from '../src/main';
 
 describe('Hello World!', () => {
   it('doit retourner "Hello World!"', () => {
