e26137ba4c375d537d1a61a7d6ec737fbed485ef
Gautier Vanneste <gautier@nopillo.com>
2024-02-28 14:12:32 +0100

    still initiating project

--- three-layers/src/main.ts
+++ three-layers/src/main.ts
@@ -1,3 +1,15 @@
 export function main() {
-  return 'Hello World!';
+  const checkInIdx = process.argv.indexOf('-in');
+  const checkOutIdx = process.argv.indexOf('-out');
+  const numberOfGuestsIdx = process.argv.indexOf('-guests');
+
+  const checkInDate = process.argv[checkInIdx + 1];
+  const checkOutDate = process.argv[checkOutIdx + 1];
+  const numberOfGuests = process.argv[numberOfGuestsIdx + 1];
+
+  console.log(`Check-in date: ${checkInDate}`);
+  console.log(`Check-out date: ${checkOutDate}`);
+  console.log(`Number of guests: ${numberOfGuests}`);
 }
+
+main();
