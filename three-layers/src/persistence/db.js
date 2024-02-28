a3e6ba64db8698ffe253ecc518d9447daae10d62
Gautier Vanneste <gautier@nopillo.com>
2024-02-28 13:28:17 +0100

    add ids to database elements

--- three-layers/src/persistence/db.js
+++ three-layers/src/persistence/db.js
@@ -1,5 +1,6 @@
 export const db = [
   {
+    id: 'a',
     number: 101,
     floor: 1,
     capacity: 2,
@@ -7,6 +8,7 @@ export const db = [
       '1 king size bed - A/C - Wi-Fi - private bathroom - wheelchair accessible'
   },
   {
+    id: 'b',
     number: 102,
     floor: 1,
     capacity: 4,
@@ -14,6 +16,7 @@ export const db = [
       '2 queen size beds - A/C - Wi-Fi - private bathroom - wheelchair accessible'
   },
   {
+    id: 'c',
     number: 103,
     floor: 1,
     capacity: 3,
@@ -21,6 +24,7 @@ export const db = [
       '1 queen size bed, 1 single size bed - A/C - Wi-Fi - private bathroom - wheelchair accessible'
   },
   {
+    id: 'd',
     number: 201,
     floor: 2,
     capacity: 4,
@@ -28,6 +32,7 @@ export const db = [
       '2 queen size beds - Wi-Fi - private bathroom - wheelchair accessible'
   },
   {
+    id: 'e',
     number: 202,
     floor: 2,
     capacity: 1,
@@ -35,6 +40,7 @@ export const db = [
       '1 single size bed - Wi-Fi - private bathroom - wheelchair accessible'
   },
   {
+    id: 'f',
     number: 203,
     floor: 2,
     capacity: 3,
@@ -42,6 +48,7 @@ export const db = [
       '1 king size bed, 1 single size bed - Wi-Fi - private bathroom - wheelchair accessible'
   },
   {
+    id: 'g',
     number: 204,
     floor: 2,
     capacity: 6,
@@ -49,6 +56,7 @@ export const db = [
       '1 king size bed, 2 queen size beds - Wi-Fi - private bathroom - wheelchair accessible'
   },
   {
+    id: 'h',
     number: 301,
     floor: 3,
     capacity: 7,
@@ -56,6 +64,7 @@ export const db = [
       '2 king size bed, 1 queen size beds, 1 single size bed - Wi-Fi - private bathroom - wheelchair accessible'
   },
   {
+    id: 'i',
     number: 302,
     floor: 3,
     capacity: 2,
